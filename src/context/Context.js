1b59223b7dec4d09df16983b49eddf92dd7728a8
ak9748564 <ak9748564@gmail.com>
2023-04-27 15:03:17 +0530

    first commit

---
+++ src/context/Context.js
@@ -0,0 +1,49 @@
+import {createContext, useState} from 'react';
+
+export const Context = createContext({
+    profileType: "",
+    changeProfileType: () => {},
+    chatProfile: "",
+    changeChatProfile: () => {},
+    isProfileOpen: "",
+    changeIsProfileOpen: () => {},
+    searchMessages: "",
+    changeSearchMessages: () => {},
+    videoCall: "",
+    changeVideoCall: () => {},
+    audioCall: "",
+    changeAudioCall: () => {},
+});
+
+export const ContextProvider = ({children}) => {
+    const [profileType, setProfileType] = useState("");
+    const [isProfileOpen, setIsProfileOpen] = useState("");
+    const [searchMessages, setSearchMessages] = useState("");
+    const [chatProfile, setChatProfile] = useState({});
+    const [videoCall, setVideoCall] = useState("");
+    const [audioCall, setAudioCall] = useState("");
+
+    const changeProfileType = (value) => {
+        setProfileType(value);
+    }
+    const changeChatProfile = (value) => {
+        setChatProfile(value);
+    }
+    const changeIsProfileOpen = (value) => {
+        setIsProfileOpen(value);
+    }
+    const changeSearchMessages = (value) => {
+        setSearchMessages(value);
+    }
+    const changeVideoCall = (value) => {
+        setVideoCall(value);
+    }
+    const changeAudioCall = (value) => {
+        setAudioCall(value);
+    }
+    return (
+        <Context.Provider value={{ profileType, changeProfileType, isProfileOpen, changeIsProfileOpen,  searchMessages, changeSearchMessages, chatProfile, changeChatProfile, videoCall, changeVideoCall, audioCall, changeAudioCall }}>
+            {children}
+        </Context.Provider>
+    )
+}
